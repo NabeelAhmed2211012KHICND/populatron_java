8b8427c16a3f273eb0fa84c311a377a88ac9b4bf
NabeelAhmed2211012KHICND <nabeel.ahmed@xloopdigital.com>
2022-12-22 10:49:05 +0500

    All Test Passed

--- src/test/java/PopulationCounterTest.java
+++ src/test/java/PopulationCounterTest.java
@@ -13,6 +13,8 @@ import static org.mockito.BDDMockito.given;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -47,10 +49,31 @@ public class PopulationCounterTest {
 
         FileReader fileReader = mock(FileReader.class);
         given(fileReader.GetReadData()).willReturn(Arrays.asList("ad,aixas,Aixs,06,,42.4833333,1.4666667"));
-        fileReader.GetReadData();
+        List<String> data = fileReader.GetReadData();
         then(fileReader).should(times(1)).GetReadData();
-        String line1 = fileReader.GetReadData().get(0);
+        String line1 = data.get(0);
         assertEquals("ad,aixas,Aixs,06,,42.4833333,1.4666667",line1);
         
+    }
+
+    @Test
+    public void canReturnParseData() throws Exception{
+        Parser parser = mock(Parser.class);
+        List<String[]> data = new LinkedList<>();
+        String[] onerow = {"ad","aixas","06","","42.4833333","1.4666667"};
+        data.add(onerow);
+        given(parser.GetParseArray()).willReturn(data);
+        List<String[]> parse_data = parser.GetParseArray();
+        then(parser).should(times(1)).GetParseArray();
+        assertEquals(data, parse_data);
+    }
+
+    @Test
+    public void canReturnSum() throws Exception{
+        Summation summation = mock(Summation.class);
+        given(summation.GetSum()).willReturn(1347982728l);
+        long sum = summation.GetSum();
+        then(summation).should(times(1)).GetSum();
+        assertEquals(1347982728l, sum);
     }
 }
